e0fff8e6307020e2fd3b1838efb2786da9fbee4f
BuXianWanYin <2644832053@qq.com>
2025-09-17 18:35:34 +0800

    first commit

---
+++ src/main/java/com/springbootdemo/filmreviewweb/FilmReviewWebApplication.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.filmreviewweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmReviewWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilmReviewWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbootdemo/filmreviewweb/FilmReviewWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdemo.filmreviewweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmReviewWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
