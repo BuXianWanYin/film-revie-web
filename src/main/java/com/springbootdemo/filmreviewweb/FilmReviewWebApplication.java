723ea325dde1d82f7db5d88e2f11bf39838746cd
BuXianWanYin <2644832053@qq.com>
2025-09-17 21:32:28 +0800

    项目优化

--- src/main/java/com/springbootdemo/filmreviewweb/FilmReviewWebApplication.java
+++ src/main/java/com/springbootdemo/filmreviewweb/FilmReviewWebApplication.java
@@ -3,6 +3,8 @@ package com.springbootdemo.filmreviewweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class FilmReviewWebApplication {
 
