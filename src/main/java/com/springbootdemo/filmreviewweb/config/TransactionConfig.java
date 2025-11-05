2c26fe6a81b4527f3508456e6b287faa8f00bee6
BuXianWanYin <2644832053@qq.com>
2025-11-05 20:36:51 +0800

    响应式优化

---
+++ src/main/java/com/springbootdemo/filmreviewweb/config/TransactionConfig.java
@@ -0,0 +1,24 @@
+package com.springbootdemo.filmreviewweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * 事务配置类
+ * 解决 MyBatis 连接同步警告问题
+ */
+@Configuration
+@EnableTransactionManagement
+public class TransactionConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
+
